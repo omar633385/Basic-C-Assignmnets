4a9194251f49c22332c686994ba1057d5c0bce8c
3omar63 <om728526@gmail.com>
2024-12-11 16:01:42 +0200

    Add project files.

---
+++ session02_assignment/Program.cs
@@ -0,0 +1,74 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+
+namespace session02_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write a program that allows the user to enter a number then print it.
+            //Console.Write("Enter number\n ");
+
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"You entered : {number}");
+
+
+            #endregion
+
+            #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+            //int x=10;
+            //int y=5;
+            //Console.WriteLine($"Before Assigning\n x={x} ,y={y}");
+
+            //y = x;
+            //Console.WriteLine($"After Assigning\n x={x} ,y={y}");
+
+
+
+            #endregion
+
+            #region Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+            //Rectangle r1;
+            //r1 = new Rectangle();
+            //r1.height = 10;
+            //r1.width = 5;
+
+            //Rectangle r2 = new Rectangle();
+           
+
+            //Console.WriteLine("Before Assigning");
+
+            //Console.WriteLine($"r1 height = {r1.height}");
+            //Console.WriteLine($"r1 width = {r1.width}");
+            //Console.WriteLine("//////////////////////////////////////////");
+            //Console.WriteLine($"r2 height = {r2.height}");
+            //Console.WriteLine($"r2 width = {r2.width}");
+            //Console.WriteLine("//////////////////////////////////////////");
+            //r2 = r1;
+            //Console.WriteLine("After Assigning");
+
+            //Console.WriteLine($"r1 height = {r1.height}"); 
+            //Console.WriteLine($"r1 width = {r1.width}");
+            //Console.WriteLine("//////////////////////////////////////////");
+            //Console.WriteLine($"r2 height = {r2.height}");
+            //Console.WriteLine($"r2 width = {r2.width}");
+
+            //r2.width = 10;
+            //Console.WriteLine("After Modifying r2");
+
+            //Console.WriteLine($"r1 height = {r1.height}");
+            //Console.WriteLine($"r1 width = {r1.width}");
+            //Console.WriteLine("//////////////////////////////////////////");
+            //Console.WriteLine($"r2 height = {r2.height}");
+            //Console.WriteLine($"r2 width = {r2.width}");
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ session02_assignment/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session02_assignment
+{
+    internal class Rectangle
+    {
+        public int width;
+        public int height;
+    }
+}
