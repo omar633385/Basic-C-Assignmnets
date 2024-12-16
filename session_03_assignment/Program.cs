c123e37a9f70694048bb178003d4e198e219f065
3omar63 <om728526@gmail.com>
2024-12-16 20:47:50 +0200

    solved q3

--- session_03_assignment/Program.cs
+++ session_03_assignment/Program.cs
@@ -18,17 +18,58 @@
             #endregion
 
             #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
-            Console.WriteLine("Enter number : ");
-            int number;
-            int.TryParse(Console.ReadLine(), out number);
-            if (number >0)
-                Console.WriteLine("Postive");
-            else
-                Console.WriteLine("Negative");
+            //Console.WriteLine("Enter number : ");
+            //int number;
+            //int.TryParse(Console.ReadLine(), out number);
+            //if (number >0)
+            //    Console.WriteLine("Postive");
+            //else
+            //    Console.WriteLine("Negative");
 
 
 
             #endregion
+
+            #region 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //Console.WriteLine("Enter 1st number : ");
+            //int number1;
+            //int.TryParse(Console.ReadLine(), out number1);
+
+            //Console.WriteLine("Enter 2nd number : ");
+            //int number2;
+            //int.TryParse(Console.ReadLine(), out number2);
+
+            //Console.WriteLine("Enter 3rd number : ");
+            //int number3;
+            //int.TryParse(Console.ReadLine(), out number3);
+
+            //if (number1 > number2 && number1 > number3)
+            //    Console.WriteLine($"max element = {number1}");
+
+            //else if (number2 > number1 && number2 > number3)
+            //        Console.WriteLine($"max element = {number2}");
+            //else if (number3 > number1 && number3 > number2)
+            //    Console.WriteLine($"max element = {number3}");
+
+            //if (number1 < number2 && number1 < number3)
+            //    Console.WriteLine($"min element = {number1}");
+
+            //else if (number2 < number1 && number2 < number3)
+            //    Console.WriteLine($"min element = {number2}");
+            //else if (number3 < number1 && number3 < number2)
+            //    Console.WriteLine($"min element = {number3}");
+
+
+
+
+            {
+
+            }
+
+
+            #endregion
+
+
         }
     }
 }
