f828d53455f54c99925684ec2db3ab5fe7bfc1f8
3omar63 <om728526@gmail.com>
2024-12-19 13:09:32 +0200

    solved q1

---
+++ session_4_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace session_4_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            //Console.WriteLine("Enter number");
+            //int input;
+            //int.TryParse(Console.ReadLine(),out input);
+            //for (int i = 1; i <=input ; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    
+            //}
+            #endregion
+
+            #region MyRegion
+
+            #endregion
+        }
+    }
+}
