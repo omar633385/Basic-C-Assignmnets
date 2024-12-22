38048c4e96bb17fd47810de88cac74fe7f52a7a6
3omar63 <om728526@gmail.com>
2024-12-22 15:55:26 +0200

    solved q1

---
+++ Session05_assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Session05_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- . Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            //Console.WriteLine("Enter Size of Array");
+            //int n;
+            //bool flag=int.TryParse(Console.ReadLine(), out n);
+           
+            //while (!flag|| n<=0) {
+
+            //    Console.WriteLine("You entered invalid input it should be int");
+            //    flag=int.TryParse(Console.ReadLine(), out n);
+            //}
+            //int[,] arr =new int[n,n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            arr[i,j] = 1;
+            //        }
+
+            //    }
+                
+            //}
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.Write(arr[i,j]+ "");
+
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            #endregion
+        }
+    }
+}
