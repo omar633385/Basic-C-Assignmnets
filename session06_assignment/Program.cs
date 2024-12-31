09a2e670ab2f8079bacee419014d5ff66e5b0860
3omar63 <om728526@gmail.com>
2024-12-31 07:00:18 +0200

    solved q2

--- session06_assignment/Program.cs
+++ session06_assignment/Program.cs
@@ -1,42 +1,67 @@
-﻿namespace session06_assignment
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace session06_assignment
 {
     internal class Program
     {
+        public static int SumDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                int remainder=number % 10;
+                sum += remainder;
+                number/=10;
+            }
+
+            return sum;
+        }
         static void Main(string[] args)
         {
 
             #region 1.	Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
 
-           //// Console.WriteLine("Passing By Value");
-           // int a=10,b=20;
-           //// Console.WriteLine($"Before Swapping a={a} b={b}");
-           // void swap(ref int a ,ref int b) {
+            //// Console.WriteLine("Passing By Value");
+            // int a=10,b=20;
+            //// Console.WriteLine($"Before Swapping a={a} b={b}");
+            // void swap(ref int a ,ref int b) {
 
-           //     int temp = a;
-           //     a = b;
-           //     b=temp;
-           // }
-           // //swap(a , b);
-           // //Console.WriteLine($"Passing By Value a={a} b={b}");
-
-
-           // //Passing by value means it takes copies of values and do the opertion once swap function stack finish
-           // //values of a,b don't change.
-           // //the change happens in swap stack and it did its job
+            //     int temp = a;
+            //     a = b;
+            //     b=temp;
+            // }
+            // //swap(a , b);
+            // //Console.WriteLine($"Passing By Value a={a} b={b}");
 
 
-           // Console.WriteLine("Passing By Reference");
-           // Console.WriteLine($"Before Swapping a={a} b={b}");
+            // //Passing by value means it takes copies of values and do the opertion once swap function stack finish
+            // //values of a,b don't change.
+            // //the change happens in swap stack and it did its job
 
-           // swap(ref a,ref b);
-           // Console.WriteLine($"Passing By Value a={a} b={b}");
 
-           // //Passing by Reference here  it takes the variable itselef and change it unlike passing by value  
-           // //values of a,b actually change.
+            // Console.WriteLine("Passing By Reference");
+            // Console.WriteLine($"Before Swapping a={a} b={b}");
+
+            // swap(ref a,ref b);
+            // Console.WriteLine($"Passing By Value a={a} b={b}");
+
+            // //Passing by Reference here  it takes the variable itselef and change it unlike passing by value  
+            // //values of a,b actually change.
 
             #endregion
 
+            #region 2.	Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number
+            //Console.WriteLine("Enter Number");
+            //int number;
+            //bool flag;
+            //do
+            //{
+            //     flag = int.TryParse(Console.ReadLine(), out number);
+            //}
+            //while (!flag);
+            //Console.WriteLine($"The sum of the digits of the number {number} is: {SumDigits(number)}");
 
+            #endregion
 
 
         }
